87494bef51272b05bf64bd5e0ced5919eca394c5
kaverma <kaverma@github.com>
2021-03-19 14:02:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> to see changes here. - Change2
+          Edit <code>src/App.js</code> to see changes here. - Change3
         </p>
         <a
           className="App-link"
